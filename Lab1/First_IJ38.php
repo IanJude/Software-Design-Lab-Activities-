d873ab99c341c84d3098cee28d6566f943e32e05
IanJude <70187809+IanJude@users.noreply.github.com>
2022-02-14 15:57:38 +0800

    Add files via upload

---
+++ Lab1/First_IJ38.php
@@ -0,0 +1,12 @@
+<html> <head> <title> PHP Test: First Example </title> </head>
+<body>
+<?php
+print "Hello World!<br>";
+print "I'm in <b>ADU! </b><br>";
+echo 'She said, "How are you?"<br>';
+print "I'm just fine.<br>";
+print "Today date is ";
+echo gmdate("M d Y");
+?>
+</body>
+<html>
